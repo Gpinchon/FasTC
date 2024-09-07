fb1539e8655a3f1508f580da847e6ba79bb3effd
gpinchon <pinchon.guillaume@gmail.com>
2024-09-07 14:16:19 +0200

    use CMake folders

--- cmake/Fetch_GoogleTest.cmake
+++ cmake/Fetch_GoogleTest.cmake
@@ -9,5 +9,17 @@ macro(Fetch_GoogleTest)
         GIT_TAG         v1.15.2
     )
     FetchContent_MakeAvailable(GTEST)
+    set_target_properties(
+      gtest
+      PROPERTIES FOLDER "3rdParty/GoogleTest")
+    set_target_properties(
+      gtest_main
+      PROPERTIES FOLDER "3rdParty/GoogleTest")
+    set_target_properties(
+      gmock
+      PROPERTIES FOLDER "3rdParty/GoogleTest")
+    set_target_properties(
+      gmock_main
+      PROPERTIES FOLDER "3rdParty/GoogleTest")
   endif (NOT TARGET gtest)
 endmacro()
